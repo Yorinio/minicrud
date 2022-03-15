6db18b2820601bf75ddacb3e97982538e60d2119
Yorinio <95612001+Yorinio@users.noreply.github.com>
2022-03-15 10:43:06 +0100

    updates

--- index.php
+++ index.php
@@ -8,20 +8,14 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Sansita+Swashed:wght@300&family=Ubuntu:wght@300&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet">
     <title>Cafetaria S</title>
 </head>
 <body>
     <!--Header-->
-    <div class="header">
-        <center><h1>Cafetaria S</h1></center>
-        <center><p>Voor de luxe lekkernij</p></center>
-        <div class="hf-left">
-            <p>0648293092 <br> cafetaria_s@gmail.com</p>
-        </div>
-        <div class="hf-right">
-            <p>Bredestraat 2 <br> 6695CA Wijchen</p>
-        </div>
-    </div>
+   
+
     <!--Navbar-->
     <nav class="nav">
         <ul>
@@ -33,9 +27,14 @@
         </ul>
     </nav>
     <!--Container-->
+
     <div class="container">
         <center><h1>Over ons</h1></center>
-        <p>Cafetaria S is een beginnende snackbar te Wijchen.</p>
+        <div class="topleftarrow"></div>
+        <div class="textbox">
+        <h5>Cafetaria S is een beginnende snackbar te Wijchen.</h5>
+
+
         <p>Hier combineren wij het gemak van fastfood met de luxe van een restaurant.</p>
         <p>In onze keuken vindt u professionele apparatuur met échte chefs. Dit is ook wat ons uniek maakt als snackbar.</p>
         <br>
@@ -43,6 +42,7 @@
         <br>
         <p>Graag verwelkomen wij u in ons restaurant op de Bredestraat 2, 6695CA Wijchen.</p>
         <p>Wilt u liever langskomen voor een take-away? Bel ons dan alstublieft op: 0648293092</p>
+    </div>
     </div>
     <!--Footer-->
     <div class="footer">
