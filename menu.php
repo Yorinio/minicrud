c7de4c5ddef85d5870489052f4323a69906bc774
Yorinio <95612001+Yorinio@users.noreply.github.com>
2022-03-15 10:41:55 +0100

    commit

---
+++ menu.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<nav class="nav">
+        <ul>
+            <li class="welkom-li"><center><h1>Menu</h1></center></li>
+            <li>Waarom Ons?</li>
+            <li>Ons Assortiment</li>
+            <li>Onze Specialiteiten</li>
+            <li>Over Ons</li>
+        </ul>
+    </nav>
+</body>
+</html>
